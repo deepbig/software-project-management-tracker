add786a8e2dec77c8b6469e011f8b3fb44bc7e23
Hongsuk Ryu <hryu@student.42.us.org>
2021-03-08 17:56:55 +0900

    initial Commit with Frontend and backend

---
+++ src/main/java/com/hryu/projecttrackerapp/projecttrackerapp/ProjecttrackerappApplication.java
@@ -0,0 +1,13 @@
+package com.hryu.projecttrackerapp.projecttrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttrackerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttrackerappApplication.class, args);
+	}
+
+}
